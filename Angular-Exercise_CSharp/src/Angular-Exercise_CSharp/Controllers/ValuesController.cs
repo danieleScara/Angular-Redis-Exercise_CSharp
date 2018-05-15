8dbecc731ce34edd2058d60d428fd791ec8dbee2
Daniele Scaramuzzi <dscaramuzzi@codearchitects.com>
2018-05-15 09:53:51 +0200

    Final integration with Redis using C#

---
+++ Angular-Exercise_CSharp/src/Angular-Exercise_CSharp/Controllers/ValuesController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServiceStack.Redis;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace Angular_Exercise_CSharp.Controllers
+{
+    public class Person {
+        
+        string name;
+        string surname;
+        string birthDate;
+        string id;
+    }
+
+    [Route("")]
+    public class ValuesController : Controller
+    {
+
+        // GET api/values
+        [HttpGet("listPersons")]
+        public JsonResult Get()
+        {
+            RedisClient redisClient = new RedisClient("localhost", 6379);
+            List<string> allKeys = redisClient.GetAllKeys();
+            List<Dictionary<string,string>> persons = new List<Dictionary<string, string>>();
+            int numKeys = allKeys.Count();
+            foreach (string item in allKeys)
+            {
+                persons.Add(redisClient.GetAllEntriesFromHash(item));
+            }
+            return Json(persons);
+        }
+
+        // POST 
+        [HttpPost("addPerson")]
+        public void Post([FromBody] dynamic person)
+        {
+            RedisClient redisClient = new RedisClient("localhost", 6379);
+            byte[][] values = new byte[4][];
+            values[0] = System.Text.Encoding.ASCII.GetBytes((string)person.id);
+            values[1] = System.Text.Encoding.ASCII.GetBytes((string)person.name);
+            values[2] = System.Text.Encoding.ASCII.GetBytes((string)person.surname);
+            values[3] = System.Text.Encoding.ASCII.GetBytes((string)person.birthDate);
+
+            string a1 = System.Text.Encoding.Default.GetString(values[0]);
+            string a2 = System.Text.Encoding.Default.GetString(values[1]);
+            string a3 = System.Text.Encoding.Default.GetString(values[2]);
+            string a4 = System.Text.Encoding.Default.GetString(values[3]);
+
+            byte[][] keys = new byte[4][];
+            keys[0] = System.Text.Encoding.ASCII.GetBytes("id");
+            keys[1] = System.Text.Encoding.ASCII.GetBytes("name");
+            keys[2] = System.Text.Encoding.ASCII.GetBytes("surname");
+            keys[3] = System.Text.Encoding.ASCII.GetBytes("birthDate");
+
+            redisClient.HMSet("person:" + person.id, keys, values);
+
+            //another way to do the same thing
+            /*redisClient.SetEntryInHashIfNotExists("person:" + person.id, "id", (string)person.id);
+            redisClient.SetEntryInHash("person:" + person.id, "name", (string)person.name);
+            redisClient.SetEntryInHash("person:" + person.id, "surname", (string)person.surname);
+            redisClient.SetEntryInHash("person:" + person.id, "birthDate", (string)person.birthDate);*/
+        }
+
+        // DELETE 
+        [HttpDelete("deletePerson/{id}")]
+        public void Delete( int id)
+        {
+            RedisClient redisClient = new RedisClient("localhost", 6379);
+            redisClient.Del("person:"+id);
+
+            //another way to do the same thing
+            /*redisClient.HDel("person:" + id, System.Text.Encoding.ASCII.GetBytes("id"));
+            redisClient.HDel("person:" + id, System.Text.Encoding.ASCII.GetBytes("name"));
+            redisClient.HDel("person:" + id, System.Text.Encoding.ASCII.GetBytes("surname"));
+            redisClient.HDel("person:" + id, System.Text.Encoding.ASCII.GetBytes("birthDate"));*/
+        }
+    }
+}
