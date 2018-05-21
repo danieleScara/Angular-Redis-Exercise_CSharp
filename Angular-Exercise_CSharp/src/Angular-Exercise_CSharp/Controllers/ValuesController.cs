bbb5942e1f32ad07c58967e59c8d8079d5f69a6c
danieleScara <dscaramuzzi@codearchitects.com>
2018-05-21 11:58:52 +0200

    Add typed objects in web api

--- Angular-Exercise_CSharp/src/Angular-Exercise_CSharp/Controllers/ValuesController.cs
+++ Angular-Exercise_CSharp/src/Angular-Exercise_CSharp/Controllers/ValuesController.cs
@@ -11,10 +11,10 @@ namespace Angular_Exercise_CSharp.Controllers
 {
     public class Person {
         
-        string name;
-        string surname;
-        string birthDate;
-        string id;
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string birthDate { get; set; }
+        public long id { get; set; }
     }
 
     [Route("")]
@@ -25,7 +25,7 @@ namespace Angular_Exercise_CSharp.Controllers
         [HttpGet("listPersons")]
         public JsonResult Get()
         {
-            RedisClient redisClient = new RedisClient("localhost", 6379);
+            /*RedisClient redisClient = new RedisClient("localhost", 6379); 
             List<string> allKeys = redisClient.GetAllKeys();
             List<Dictionary<string,string>> persons = new List<Dictionary<string, string>>();
             int numKeys = allKeys.Count();
@@ -33,14 +33,21 @@ namespace Angular_Exercise_CSharp.Controllers
             {
                 persons.Add(redisClient.GetAllEntriesFromHash(item));
             }
-            return Json(persons);
+            return Json(persons);*/
+
+            using (IRedisClient client = new RedisClient())
+            {
+                var personClient = client.As<Person>();
+                var persons = personClient.GetAll();
+                return Json(persons);
+            }
         }
 
         // POST 
         [HttpPost("addPerson")]
         public void Post([FromBody] dynamic person)
         {
-            RedisClient redisClient = new RedisClient("localhost", 6379);
+            /*RedisClient redisClient = new RedisClient("localhost", 6379);
             byte[][] values = new byte[4][];
             values[0] = System.Text.Encoding.ASCII.GetBytes((string)person.id);
             values[1] = System.Text.Encoding.ASCII.GetBytes((string)person.name);
@@ -58,7 +65,13 @@ namespace Angular_Exercise_CSharp.Controllers
             keys[2] = System.Text.Encoding.ASCII.GetBytes("surname");
             keys[3] = System.Text.Encoding.ASCII.GetBytes("birthDate");
 
-            redisClient.HMSet("person:" + person.id, keys, values);
+            redisClient.HMSet("person:" + person.id, keys, values);*/
+
+            using (IRedisClient client = new RedisClient())
+            {
+                var personClient = client.As<Person>();
+                personClient.Store(new Person() { id = (long)person.id, name = person.name, surname = person.surname, birthDate = person.birthDate });
+            }
 
             //another way to do the same thing
             /*redisClient.SetEntryInHashIfNotExists("person:" + person.id, "id", (string)person.id);
@@ -71,8 +84,15 @@ namespace Angular_Exercise_CSharp.Controllers
         [HttpDelete("deletePerson/{id}")]
         public void Delete( int id)
         {
-            RedisClient redisClient = new RedisClient("localhost", 6379);
-            redisClient.Del("person:"+id);
+            /*RedisClient redisClient = new RedisClient("localhost", 6379);
+            redisClient.Del("person:"+id);*/
+
+            using (IRedisClient client = new RedisClient())
+            {
+                var personClient = client.As<Person>();
+                var person = personClient.GetById(id);
+                personClient.Delete(person);
+            }
 
             //another way to do the same thing
             /*redisClient.HDel("person:" + id, System.Text.Encoding.ASCII.GetBytes("id"));
